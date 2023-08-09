090cfc248be4b9943600400fdd4a2348cdede508
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-08-09 23:21:53 +0200

    Update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      tempora, voluptatibus voluptatem perferendis autem dolorum dolorem
+      adipisci, porro tenetur voluptatum voluptas eveniet alias rerum mollitia
+      eos ducimus incidunt saepe rem eius nulla. Tempora voluptate temporibus
+      vitae fugit? Quam dolor harum vitae facere, pariatur numquam repellendus
+      repellat sapiente ratione commodi at?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      tempora, voluptatibus voluptatem perferendis autem dolorum dolorem
+      adipisci, porro tenetur voluptatum voluptas eveniet alias rerum mollitia
+      eos ducimus incidunt saepe rem eius nulla. Tempora voluptate temporibus
+      vitae fugit? Quam dolor harum vitae facere, pariatur numquam repellendus
+      repellat sapiente ratione commodi at?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      tempora, voluptatibus voluptatem perferendis autem dolorum dolorem
+      adipisci, porro tenetur voluptatum voluptas eveniet alias rerum mollitia
+      eos ducimus incidunt saepe rem eius nulla. Tempora voluptate temporibus
+      vitae fugit? Quam dolor harum vitae facere, pariatur numquam repellendus
+      repellat sapiente ratione commodi at?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      tempora, voluptatibus voluptatem perferendis autem dolorum dolorem
+      adipisci, porro tenetur voluptatum voluptas eveniet alias rerum mollitia
+      eos ducimus incidunt saepe rem eius nulla. Tempora voluptate temporibus
+      vitae fugit? Quam dolor harum vitae facere, pariatur numquam repellendus
+      repellat sapiente ratione commodi at?
+    </p>
   </div>
 </template>
