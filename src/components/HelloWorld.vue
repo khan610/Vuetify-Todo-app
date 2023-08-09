10d67829ee8ef3eda89cebe7103691e765ba43cb
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-08-09 22:18:20 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page padding>
+    <input v-model="message" @keyup="handleKeyup" />
+    <button @click="clearMessage">Clear</button>
+    <h5>{{ message }}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'I love Vue.js so hard!',
+    };
+  },
+  methods: {
+    clearMessage() {
+      this.message = '';
+    },
+    handleKeyup(e) {
+      console.log(e);
+    },
+  },
+};
+</script>
+
+<style></style>
