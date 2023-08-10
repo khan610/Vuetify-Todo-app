c3c86d3258cfff04a2c61d440848fa5bd23458c7
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-08-10 18:57:08 +0200

    Update

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,11 +1,60 @@
 <template>
-  <div class="home pa-6">
-    <h1>Todo Page</h1>
+  <div class="home">
+    <div v-for="task in tasks" :key="task.id">
+      <v-list class="pt-0" flat>
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+              >
+                {{ task.title }}</v-list-item-title
+              >
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Wake up',
+          done: false,
+        },
+        {
+          id: 2,
+          title: 'Get banans',
+          done: false,
+        },
+        {
+          id: 3,
+          title: 'Eat bananas',
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
+  },
 };
 </script>
