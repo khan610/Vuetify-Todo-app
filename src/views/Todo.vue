828f48636349503674dd4a765ebbf914fd17b5f0
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-08-11 01:06:25 +0200

    Update

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+    >
+    </v-text-field>
     <div v-for="task in tasks" :key="task.id">
       <v-list class="pt-0" flat>
         <v-list-item
@@ -37,26 +49,36 @@ export default {
   name: 'Home',
   data() {
     return {
+      newTaskTitle: '',
       tasks: [
-        {
-          id: 1,
-          title: 'Wake up',
-          done: false,
-        },
-        {
-          id: 2,
-          title: 'Get banans',
-          done: false,
-        },
-        {
-          id: 3,
-          title: 'Eat bananas',
-          done: false,
-        },
+        // {
+        //   id: 1,
+        //   title: 'Wake up',
+        //   done: false,
+        // },
+        // {
+        //   id: 2,
+        //   title: 'Get banans',
+        //   done: false,
+        // },
+        // {
+        //   id: 3,
+        //   title: 'Eat bananas',
+        //   done: false,
+        // },
       ],
     };
   },
   methods: {
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false,
+      };
+      this.tasks.push(newTask);
+      this.newTaskTitle = '';
+    },
     doneTask(id) {
       let task = this.tasks.filter((task) => task.id === id)[0];
       task.done = !task.done;
