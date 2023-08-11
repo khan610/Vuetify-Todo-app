07d4a502baf98031ab2a0635cf530bf4ba3f5851
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-08-11 18:52:06 +0200

    Update

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,35 +2,8 @@
   <div class="home">
     <field-add-task />
 
-    <v-list v-if="$store.state.tasks.length" class="pt-0" flat>
-      <div v-for="task in $store.state.tasks" :key="task.id">
-        <v-list-item
-          @click="$store.commit('doneTask', task.id)"
-          :class="{ 'blue lighten-5': task.done }"
-        >
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-            </v-list-item-action>
+    <list-tasks v-if="$store.state.tasks.length"></list-tasks>
 
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-              >
-                {{ task.title }}</v-list-item-title
-              >
-            </v-list-item-content>
-
-            <v-list-item-action>
-              <v-btn @click.stop="$store.commit('deleteTask', task.id)" icon>
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
     <div v-else class="no-tasks">
       <v-icon size="100" color="primary">mdi-check</v-icon>
       <div class="text-h5 primary--text">No tasks</div>
@@ -43,6 +16,7 @@ export default {
   name: 'Home',
   components: {
     'field-add-task': require('@/components/Todo/FieldAddTask.vue').default,
+    'list-tasks': require('@/components/Todo/ListTasks.vue').default,
   },
 };
 </script>
