6e2064dd27dca869253a77cd95a457c916553ab1
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-08-12 23:20:24 +0200

    Update

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -13,18 +13,17 @@
           <v-list-item-title
             :class="{ 'text-decoration-line-through': task.done }"
           >
-            {{ task.title }}</v-list-item-title
-          >
+            {{ task.title }}
+          </v-list-item-title>
         </v-list-item-content>
 
         <v-list-item-action>
-          <v-btn @click.stop="dialogs.delete = true" icon>
-            <v-icon color="primary lighten-1">mdi-delete</v-icon>
-          </v-btn>
+          <task-menu />
         </v-list-item-action>
       </template>
     </v-list-item>
     <v-divider></v-divider>
+
     <dialog-delete
       v-if="dialogs.delete"
       @close="dialogs.delete = false"
@@ -46,6 +45,7 @@ export default {
   components: {
     'dialog-delete': require('@/components/Todo/Dialogs/DialogDelete.vue')
       .default,
+    'task-menu': require('@/components/Todo/TaskMenu.vue').default,
   },
 };
 </script>
